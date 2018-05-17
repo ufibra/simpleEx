6c73235c936790d4937f7569e6f07d65e2b9efde
Joao Paulo <joaoded@hotmail.com>
2018-05-17 09:15:34 -0300

    Few editions to make new request

--- courses/spiders/udacity.py
+++ courses/spiders/udacity.py
@@ -10,15 +10,12 @@ class UdacitySpider(scrapy.Spider):
         divs = response.xpath("/html/body/ir-root/ir-content/ir-course-catalog/section/div/div/div/div/div")
         for div in divs:
             link = div.xpath('.//h3/a')
-            title = link.xpath('./text()').extract_first()
             href = link.xpath('./@href').extract_first()
-            # we want the img that contains className img-responsive
-            img = div.xpath('.//img[contains(@class, "img-responsive")]/@src').extract_first()
-            description = div.xpath('.//div[2]/div[2]/text()').extract_first()
             # yield is a parcial and gradual return
-            yield {
-                'title': title,
-                'url': href,
-                'image': img,
-                'description': description
-            }
+            yield scrapy.Request(
+                url = 'https://br.udacity.com%s' % href,
+                callback = self.parse_detail
+            )
+
+    def parse_detail(self, response):
+        pass
